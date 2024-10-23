384ad61e58c860497be91a04f0fd1adfe7ff964f
candylxy95 <lee.ydnac@gmail.com>
2024-10-23 18:40:53 +0800

    Added delUserLanguage

--- src/components/UserLanguage.jsx
+++ src/components/UserLanguage.jsx
@@ -28,13 +28,45 @@ const UserLanguage = (props) => {
     }
   };
 
+  const delUserLanguage = async (idx) => {
+    try {
+      const res = await fetch(
+        import.meta.env.VITE_SERVER + "/lab/users/languages",
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user_id: props.id,
+            language: userLanguages[idx],
+          }),
+        }
+      );
+      if (!res.ok) {
+        throw new Error("getting data error");
+      }
+      const data = await res.json();
+      setUserLanguages(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   useEffect(() => {
     getUserLanguages();
   }, [userLanguages]);
 
   return (
     <>
-      <p>{userLanguages}</p>
+      {userLanguages.map((userLanguage, idx) => {
+        return (
+          <p key={idx}>
+            {userLanguage}
+            <button onClick={() => delUserLanguage(idx)}>Delete</button>
+          </p>
+        );
+      })}
     </>
   );
 };
